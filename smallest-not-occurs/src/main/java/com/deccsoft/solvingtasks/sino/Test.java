c5387dbd197468b959a243d556c4b53b9feb56b4
Douglas Carvalho <douglas@decc.com.br>
2019-09-03 01:56:07 -0300

    Add main test

---
+++ smallest-not-occurs/src/main/java/com/deccsoft/solvingtasks/sino/Test.java
@@ -0,0 +1,11 @@
+package com.deccsoft.solvingtasks.sino;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int [] array = {45,12,85,32,89,39,69,44,42,1,6,8};
+		Solution solution = new Solution();
+		System.out.println(solution.solution(array));
+	}
+
+}
